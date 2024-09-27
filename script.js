f234004a226ef6a1292ab10858d43abd22fdd43d
ryanl7989 <ryanl308@nycstudents.net>
2024-09-27 14:11:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Ryan Lin"; // update the part BETWEEN the "quotes"
+let userName = "ryanl7989"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
